97c73e50cdbd66833a59c50ff8d90d4dcec89487
enzo@ieee.org <enzo@ieee.org>
2026-05-13 18:54:01 -0400

    USB.DB

---
+++ USB.DB/Query.h
@@ -0,0 +1,14 @@
+///
+/// Query.h - interfaz para los ejercicios del examen
+///
+
+#pragma once
+
+struct Query {
+    static int q0();
+    static int q1();
+    static int q2();
+    static int q3();
+    static int q4();
+    static int q5();
+};
